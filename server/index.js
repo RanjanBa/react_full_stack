22e9c2ebafd093ee0323a8b9bb9e2fb731dcb999
RanjanBa <ranjanbaro2015@gmail.com>
2019-06-07 12:05:49 +0530

    updated backend code for deployment

--- server/index.js
+++ server/index.js
@@ -15,6 +15,13 @@ app.use(bodyParser.json());
 app.use('/api/cities', require('./api/cities'));
 app.use('/api/weather', require('./api/weather'));
 
+if(ENv === 'production') {
+    app.use(express.static(path.join(__dirname, '../react_weather_app/build')));
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname, '../react_weather_app/build/index.html'));
+    });
+}
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
 });
